33ff6f1b20ae7cedbc66587c83e827cf027612ed
G00422935@atu.ie <G00422935@atu.ie>
2023-10-17 13:35:56 +0100

    Added constructors for firstname and lastname and  age

--- src/main/java/ie/atu/person.java
+++ src/main/java/ie/atu/person.java
@@ -5,6 +5,13 @@ public class person {
     String lastName;
     int age;
 
+    public person(String firstname, String lastName, int age) {
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
     public void displayInfo() {
         System.out.println("name: " + firstName + "" + lastName + ", Age: " + age);
     }
